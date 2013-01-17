259c5b78eed1f58491bfa84a87cced18b061b397
Tom <tommewett007@gmail.com>
2013-01-17 18:26:52 +0000

    Condensed whitespace & comments

--- frostyscout.py
+++ frostyscout.py
@@ -4,12 +4,9 @@ import json
 import re
 
 def esend(message):
-#   print(message)
     message += "\r\n"
     s.sendall(message.encode())
-    
 def msend(message):
-#   print(message)
     message = "PRIVMSG %s :%s\r\n" % (CHAN, message)
     s.sendall(message.encode())
 
@@ -26,31 +23,23 @@ s.connect((HOST, PORT))
 esend("NICK %s" % NICK)
 esend("USER %s %s bla :%s" % (IDENT, HOST, REALNAME))
 print("Logged in.")
-
 while 1:
     readbuffer=readbuffer+s.recv(1024).decode()
     temp=readbuffer.split("\n")
     readbuffer=temp.pop()
-
     for line in temp:
         line=line.rstrip()
 #       print(line)
-
         if line[0:4]=="PING":
             esend("PONG %s" % line[6:])
             print("Sent PONG.")
-
         elif "/MOTD" in line:
             esend('JOIN '+CHAN)
             print("Joining %s..." % CHAN)
-
         elif ':!' in line:
-            
             cmd = re.search(r":!(\w+) ?(.+)?$", line)
-            
             if cmd.group(1)=="snowman":
                 msend("Kill it with fire!")
-                
             elif cmd.group(1)=="stalk":
                 try:
                     p = json.loads(urllib.request.urlopen("https://api.kag2d.com/player/%s/status" % cmd.group(2)).read().decode())
